afc3305e58497bbda9ab0f22a9650851abd401bd
DuncanK2027 <duncan.karcher2027@gbwl.org>
2026-04-08 15:45:45 +0000

    variables

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+//This is a single LINE COMMENT
+
+/* This is a MULTI LINE COMMENT
+The code below is an example STATEMENT (instruction/command) Statements end with a SEMICOLON */
+
+console.log("Welcome to JS!");
+
+/* A VARIABLE is a named storage container for data (information) */
+
+// DECLARE a new variable (make space, give it a name/label)
+let username; 
+
+// ASSIGN a value (data) to the variable 
+username  = "duncan";
+
+// see the value stored in the variable
+console.log(username);
+
+// DECLARE + ASSIGN in one step
+let luckyNum = 13;
